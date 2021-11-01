ce01c7736ce94e443f6fbd0d8c8184e0c13edec1
chris <cdresher2010@gmail.com>
2021-11-01 16:10:18 -0400

    add definitions for new methods and got solargraph to work

--- board.rb
+++ board.rb
@@ -1,4 +1,17 @@
 class Board
+    attr_reader :rows
+
     def initialize()
+        @rows = Array.new(8) {Array.new(8)}
+    end
+
+    def [](posArray)
+
+    end
+
+    def []=(posArray)
+    end
+
+    def move_piece(startPos, endPos)
     end
 end
