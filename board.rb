d84c3b5a6ca3b0fe4a62083b88ebbb068098b295
chris <cdresher2010@gmail.com>
2021-11-01 21:36:24 -0400

    created initial methods for Board class and set up piece class

--- board.rb
+++ board.rb
@@ -1,17 +1,38 @@
+require_relative 'piece'
+
 class Board
-    attr_reader :rows
+    attr_reader :board
 
-    def initialize()
-        @rows = Array.new(8) {Array.new(8)}
+    def initialize
+        @board = Array.new(8) { Array.new(8) }
+        setup_board
     end
 
-    def [](posArray)
-
+    def setup_board
+        starting_rows = [0, 1, 6, 7]
+        @board.each_with_index do |row, row_index|
+            if starting_rows.include?(row_index)
+                row.each_index { |tile_index| row[tile_index] = Piece.new }
+            end
+        end
     end
 
-    def []=(posArray)
+    def [](pos_array)
+        row, col = pos_array
+        @board[row][col]
     end
 
-    def move_piece(startPos, endPos)
+    def []=(pos_array, val)
+        row, col = pos_array
+        @board[row][col] = val
+    end
+
+    def move_piece(start_pos, end_pos)
+        raise 'no piece is here' if self[start_pos].nil?
+        raise 'there is already a piece here' if self[end_pos]
+        # raise error if piece cannot move to end position
+        piece = self[start_pos]
+        self[start_pos] = nil
+        self[end_pos] = piece
     end
 end
--- piece.rb
+++ piece.rb
@@ -1,2 +1,15 @@
+require_relative 'board.rb'
+
 class Piece
+    attr_reader :position, :board, :color
+
+    def initialize(position, board, color)# color is how we're going to track sides
+        @position = position
+        @board = board
+        @color = color
+    end
+
+    def moves
+        #this returns all viable moves for a piece
+    end
 end
