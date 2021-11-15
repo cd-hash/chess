1b1ba88af2892730f103d7c56e209e9b3496a4e4
chris <cdresher2010@gmail.com>
2021-11-14 20:51:11 -0500

    started working on display and cursor classes

---
+++ display.rb
@@ -0,0 +1,10 @@
+require 'colorize'
+require_relative 'cursor'
+require_relative 'board'
+
+class Display
+    def initialize(board)
+        @board = board
+        @cursor = Cursor.new([0,0], board)
+    end
+end
